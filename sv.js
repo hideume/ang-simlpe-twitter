e465d3f4b72400d8f16e78b8386e41bad2c7fae5
hidehiro umedu <hide3@ac.auone-net.jp>
2022-05-01 10:46:54 +0900

    footer解決

---
+++ sv.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app  = express();
+//const port = 22512;
+let port = process.env.PORT;
+
+// POSTのクエリーを良い感じに処理する
+app.use(express.static('./dist/tw2'));
+
+// ルーティングの設定
+/*
+app.get("/", (req, res) =>{
+  const name = req.body.name;
+  res.send(`君の名は ${name}`);
+  console.log("/ へアクセスがありました");
+});
+*/
+
+// HTTPサーバを起動する
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
